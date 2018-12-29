f90c8c496d7c33b68b9972d12e9ad226d1816d8c
Kayode Adeola <monsieurkayode@gmail.com>
2018-12-29 23:44:56 +0100

    Customized portfolio routes and handled 404s

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  def not_found
+    render file: 'public/404.html', status: :not_found
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,19 @@
 Rails.application.routes.draw do
-  resources :portfolios
+  resources :portfolios, except: %i[show edit update destroy]
+  get '/portfolio/:id/edit', to: 'portfolios#edit', as: 'edit_portfolio'
+  scope '/portfolio/:id', as: 'portfolio' do
+    get '/', to: 'portfolios#show'
+    put '/', to: 'portfolios#update'
+    patch '/', to: 'portfolios#update'
+    delete '/', to: 'portfolios#destroy'
+  end
 
   get 'about-me', to: 'pages#about', as: 'about'
   get 'contact', to: 'pages#contact'
 
   resources :blogs
+
   root to: 'pages#home'
+
+  match '/(*url)', to: 'application#not_found', via: :all
 end
