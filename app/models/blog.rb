ee951dcc3b3b90e0507a2c4648f260b98e830c37
Kayode Adeola <monsieurkayode@gmail.com>
2019-01-01 02:40:37 +0100

    Integrated database relationship between topic and blog posts

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,7 +1,12 @@
 class Blog < ApplicationRecord
   enum status: { draft: 0, published: 1 }
+
   extend FriendlyId
   friendly_id :title, use: :slugged
+
   validates_presence_of :title, :body
+
+  belongs_to :topic
+
   default_scope { friendly }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_30_152031) do
+ActiveRecord::Schema.define(version: 2019_01_01_010645) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,9 @@ ActiveRecord::Schema.define(version: 2018_12_30_152031) do
     t.datetime "updated_at", null: false
     t.string "slug"
     t.integer "status", default: 0
+    t.bigint "topic_id"
     t.index ["slug"], name: "index_blogs_on_slug", unique: true
+    t.index ["topic_id"], name: "index_blogs_on_topic_id"
   end
 
   create_table "friendly_id_slugs", id: :serial, force: :cascade do |t|
@@ -56,4 +58,11 @@ ActiveRecord::Schema.define(version: 2018_12_30_152031) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "topics", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "blogs", "topics"
 end
