94503820050262c6cdf7be59131c116e3d3dc57c
Kayode Adeola <monsieurkayode@gmail.com>
2019-01-07 18:32:03 +0100

    - Extend devise session controller create and destroy actions functionality - Generate devise view for users scope to be used for session creation - Remove devise default sessions view - Configure devise route with new sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users,
+             controllers: { sessions: 'users/sessions' },
              path: '',
              path_names: {
                sign_in: 'login',
