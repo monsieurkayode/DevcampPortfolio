524048e56be81f1a3277d47d0630f070df59e837
Kayode Adeola <monsieurkayode@gmail.com>
2019-01-07 22:30:35 +0100

    Compelete authentication by using virtual attributes for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :trackable
+  validates_presence_of :name
+
+  def first_name
+    name.split(' ').first
+  end
+
+  def last_name
+    name.split(' ').last
+  end
 end
